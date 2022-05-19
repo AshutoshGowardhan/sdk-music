e2c1db77ba31d169587841d262b0485db219725a
Ashutosh <ashutosh@hyperstate.tech>
2022-05-19 23:11:37 +0530

    Feat: Inject Youtube Video

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as querString from "query-string";
 export class MusicLog {
   orange: string = "orange";
   blue: string = "blue";
@@ -22,6 +23,30 @@ export class MusicLog {
     });
   }
 
+  injectYoutubeIframe(node: HTMLElement) {
+    // const youtubeId = querString.parseUrl(youtubeLink).query["v"];
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/JYs_94znYy0`;
+    iframe.width = "400";
+    iframe.height = "100";
+    // iframe.frameBorder ="0";
+    // iframe.scrolling = "0";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    // (
+    //   <iframe
+    //     width="560"
+    //     height="315"
+    //     src=`https://www.youtube.com/embed/${youtubeId}`
+    //     title="YouTube video player"
+    //     frameborder="0"
+    //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    //     allowfullscreen
+    //   ></iframe>
+    // );
+    node.append(iframe);
+  }
+
   onMutation(mutations: MutationRecord[]) {
     console.log(mutations);
 
@@ -32,7 +57,8 @@ export class MusicLog {
           continue; // not an element
         } else {
           if (nodeL.classList.contains("superelement")) {
-            console.log(node);
+            console.log(node, nodeL);
+            this.injectYoutubeIframe(nodeL);
           }
         }
       }
