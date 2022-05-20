0ce72858feeb7495c774c9e867de389b3d956bbc
Ashutosh <ashutosh@hyperstate.tech>
2022-05-20 20:22:17 +0530

    Feat: Added DOM loaded event listner

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,31 @@ import { MusicLog } from "./music-log";
 // import canUseDom from "can-use-dom";
 
 function init() {
-  const node = document.querySelectorAll("[data-kappa]");
-  console.log(node);
+  // const node = document.querySelectorAll("[data-kappa]");
+  // console.log(node);
+  window.addEventListener("DOMContentLoaded", () => {
+    // DOM ready! Images, frames, and other subresources are still downloading.
+    const node = document.querySelectorAll("[data-kappa]");
+    console.log(node);
+  });
 }
+
+// const initDOMLoadedElements = function () {
+//   document.removeEventListener("DOMContentLoaded", initDOMLoadedElements);
+//   window.removeEventListener("load", initDOMLoadedElements);
+
+//   Array.prototype.forEach.call(
+//     document.querySelectorAll("[data-kappa]"),
+//     (el) => {
+//       console.log(el);
+//       // if (
+//       //   el.getAttribute("data-simplebar") !== "init" &&
+//       //   !SimpleBar.instances.has(el)
+//       // )
+//       //   new SimpleBar(el, getOptions(el.attributes));
+//     }
+//   );
+// };
 
 init();
 
